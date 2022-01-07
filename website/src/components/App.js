bbcc7d55fb4d406f7513938fa28cf2fc73f92fa4
mayukhsharmaaa <mayukh6801@gmail.com>
2022-01-07 23:53:37 +0530

    updated some more features

--- website/src/components/App.js
+++ website/src/components/App.js
@@ -5,14 +5,24 @@ function App() {
 
   const[mealData,setMealData]=useState(null);
   const[calories,setCalories]=useState(null);
+  const[diet,setDiet]=useState(null);
+  const[exclude,setExclude]=useState(null);
 
   function handleChange(e){
     setCalories(e.target.value);                            
   }
 
+  function handleDiet(e){
+    setDiet(e.target.value);
+  }
+  
+  function handleExclude(e){
+    setExclude(e.target.value);
+  }
+
   function getMealData(){
   fetch(
-    `https://api.spoonacular.com/mealplanner/generate?apiKey=bae565de28754165ae8db829409212f2&timeFrame=day&targetCalories=${calories}`
+    `https://api.spoonacular.com/mealplanner/generate?apiKey=bae565de28754165ae8db829409212f2&timeFrame=day&targetCalories=${calories}&diet=${diet}&exclude=${exclude}`
   ).then((response)=>response.json()).then((data)=>{
     setMealData(data);
     console.log(data);
@@ -23,11 +33,40 @@ function App() {
 
   return (
   <div className="container">
+  {/* <section className="names">
+    <p>Enter Calories (e.g. 2000)</p>
+    <p>Enter ingredients to exclude</p>
+    <p>Choose a Diet</p>
+  </section> */}
   <section className="controls">
-   <input onChange={handleChange} className="calories" type="number" placeholder="Enter Calories (e.g. 2000)" />
-  
-   <button onClick={getMealData} className="getplan">Get daily meal plan</button>
+  <section className="col">
+  <p>Enter the amount of Calories</p>
+   <input onChange={handleChange} className="calories" type="number" placeholder="Intake of Calories for the Day (e.g. 2000)" />
    </section>
+   <section className="col">
+  <p>Enter ingredients to exclude</p>
+   <input onChange={handleExclude} className="exclude" type="text" placeholder="Ingredients to exclude from Diet (e.g. Milk, Egg)" />
+  </section>
+   {/* <label for="diet" className="dietlabel">Choose a Diet:</label> */}
+    <section className="col">
+  <p>Choose a Diet</p>
+<select name="diet" className="diet" onChange={handleDiet}>
+  <option value="volvo">Gluten Free</option>
+  <option value="saab">Ketogenic</option>
+  <option value="mercedes">Vegetarian</option>
+  <option value="audi">Lacto-Vegetarian</option>
+  <option value="audi">Ovo-Vegetarian</option>
+  <option value="audi">Vegan</option>
+  <option value="audi">Pescetarian</option>
+  <option value="audi">Paleo</option>
+  <option value="audi">Primal</option>
+  <option value="audi">Low FODMAP</option>
+  <option value="audi">Whole30</option>
+</select>
+</section>
+</section>
+   <button onClick={getMealData} className="getplan">Get daily meal plan</button>
+   
    {mealData && <MealList mealData={mealData} />}
   </div>
   );
